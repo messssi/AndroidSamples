ae08f04ce04d75e44dc3f5ee3235ba30de551d42
messssi <flyingdonkey.e@gmail.com>
2020-08-02 17:55:27 +0900

    8.4 コンテキストメニュー

---
+++ MenuSample/IListExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MenuSample
+{
+
+    public static class IListExtensions
+    {
+        /// <summary>
+        /// 指定されたインデックスに要素が存在する場合に true を返します
+        /// https://baba-s.hatenablog.com/entry/2015/04/01/104854
+        /// </summary>
+        public static bool IsDefinedAt<T>(this IList<T> self, int index)
+        {
+            return index < self.Count;
+        }
+    }
+}
--- MenuSample/MainActivity.cs
+++ MenuSample/MainActivity.cs
@@ -14,10 +14,25 @@ namespace MenuSample
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        /// <summary>
+        /// 表示メニューリスト
+        /// </summary>
         private List<IDictionary<string, object>> _menuList = null;
+
+        /// <summary>
+        /// アダプターに渡すための定義(表示用)
+        /// </summary>
         private string[] _from;
+
+        /// <summary>
+        /// アダプターに渡すための定義(表示用)
+        /// </summary>
         private int[] _to;
 
+        /// <summary>
+        /// アクティビティ生成時の処理
+        /// </summary>
+        /// <param name="savedInstanceState"></param>
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -40,8 +55,15 @@ namespace MenuSample
 
             // クリックイベントハンドラ登録
             lvMenu.ItemClick += OnListItemClick;
+
+            // コンテキストメニューの登録
+            this.RegisterForContextMenu(lvMenu);
         }
 
+        /// <summary>
+        /// データ生成(定食)
+        /// </summary>
+        /// <returns></returns>
         private static List<IDictionary<string, object>> CreateTeishokuList()
         {
             var data = new List<(string name, decimal price, string desc)>()
@@ -63,6 +85,10 @@ namespace MenuSample
             return menuList;
         }
 
+        /// <summary>
+        /// データ生成(カレー)
+        /// </summary>
+        /// <returns></returns>
         private static List<IDictionary<string, object>> CreateCurryList()
         {
             var data = new List<(string name, decimal price, string desc)>()
@@ -83,10 +109,25 @@ namespace MenuSample
             return menuList;
         }
 
+        /// <summary>
+        /// リストアイテム選択時の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OnListItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
-            //タップされた行のデータ取得
+            // タップされた行のデータ取得
             var item = e.Parent.GetItemAtPosition(e.Position) as IDictionary<string, object>;
+            // 注文処理
+            Order(item);
+        }
+
+        /// <summary>
+        /// 注文処理
+        /// </summary>
+        /// <param name="item"></param>
+        private void Order(IDictionary<string, object> item)
+        {
             var name = item[NAME_KEY] as string;
             var price = (decimal)item[PRICE_KEY];
 
@@ -97,6 +138,11 @@ namespace MenuSample
             this.StartActivity(intent);
         }
 
+        /// <summary>
+        /// オプションメニュー生成時の処理
+        /// </summary>
+        /// <param name="menu"></param>
+        /// <returns></returns>
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             // 追加直後はResource。Menu が無い旨エラーになったが、
@@ -105,6 +151,11 @@ namespace MenuSample
             return base.OnCreateOptionsMenu(menu);
         }
 
+        /// <summary>
+        /// オプションメニュー選択時の処理
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             //選択メニューIDによりデータを差し替える
@@ -124,6 +175,57 @@ namespace MenuSample
                 Resource.Layout.row, _from, _to);
             lvMenu.Adapter = adapter;
             return base.OnOptionsItemSelected(item);
+        }
+
+        /// <summary>
+        /// コンテキストメニュー生成時の処理
+        /// </summary>
+        /// <param name="menu"></param>
+        /// <param name="v"></param>
+        /// <param name="menuInfo"></param>
+        public override void OnCreateContextMenu(IContextMenu menu, View v, IContextMenuContextMenuInfo menuInfo)
+        {
+            base.OnCreateContextMenu(menu, v, menuInfo);
+
+            // コンテキストメニューのレイアウトファイルをインフレ―ト、タイトル設定
+            this.MenuInflater.Inflate(Resource.Menu.menu_context_menu_list, menu);
+            menu.SetHeaderTitle(Resource.String.menu_list_context_header);
+        }
+
+        /// <summary>
+        /// コンテキストメニュー選択時の処理
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public override bool OnContextItemSelected(IMenuItem item)
+        {
+            //情報格納オブジェクト取得
+            var info = item.MenuInfo as AdapterView.AdapterContextMenuInfo;
+            //長押しされたポジション取得→情報取得
+            var listPosition = info.Position;
+            var isDefined = this._menuList?.IsDefinedAt(listPosition) ?? false;
+            var menu = isDefined ? _menuList[listPosition] : null;
+            if (menu == null)
+            {
+                return false;
+            }
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.menuListContextDesc:
+                    // 説明メニュー
+                    var desc = menu[DESC_KEY] as string;
+                    Toast.MakeText(this.ApplicationContext, desc, ToastLength.Long).Show();
+                    break;
+                case Resource.Id.menuListContextOrder:
+                    // 注文メニュー
+                    this.Order(menu);
+                    break;
+                default:
+                    break;
+            }
+
+            return base.OnContextItemSelected(item);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
